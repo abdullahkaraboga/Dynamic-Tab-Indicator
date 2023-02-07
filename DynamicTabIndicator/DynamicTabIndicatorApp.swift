745403e5ad9366be69c376e6f2602c8bce251806
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-07 23:34:35 +0300

    Initial Commit

---
+++ DynamicTabIndicator/DynamicTabIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabIndicatorApp.swift
+//  DynamicTabIndicator
+//
+//  Created by Abdullah Karaboğa on 7.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
